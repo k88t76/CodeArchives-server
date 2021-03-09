579109deb316055de2d1101f50345c7d235d976b
k88t76 <k88t76@outlook.jp>
2021-03-09 20:50:01 +0900

    add domain on cookie

--- controllers/server.go
+++ controllers/server.go
@@ -305,7 +305,7 @@ func setCookie(w http.ResponseWriter, r *http.Request) {
 		HttpOnly: true,
 		Secure:   true,
 		SameSite: http.SameSiteNoneMode,
-		Domain:   "example.app",
+		Domain:   "vercel.app",
 	}
 	http.SetCookie(w, cookie)
 	/* クッキー削除
