016a45ec70154add3f1c79b38e81a23b1f61b9f3
k88t76 <k88t76@outlook.jp>
2021-03-17 00:58:04 +0900

    add archive handler unit testing

--- controllers/archive/archive_test.go
+++ controllers/archive/archive_test.go
@@ -2,6 +2,7 @@ package archive
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -98,4 +99,99 @@ func Test_postArchive(t *testing.T) {
 	if !reflect.DeepEqual(archive, expect) {
 		t.Errorf("archive must be %v but %v", expect, archive)
 	}
+}
+
+func Test_putArchive(t *testing.T) {
+	db := sqlx.MustConnect("mysql", config.Config.DbAccess+"code_archives_test?parseTime=true&loc=Asia%2FTokyo")
+	defer func() {
+		// DBのCleanup
+		db.MustExec("set foreign_key_checks = 0")
+		db.MustExec("truncate table archives")
+		db.MustExec("set foreign_key_checks = 1")
+		db.Close()
+	}()
+
+	origin := models.Archive{
+		UUID:     "uuid1",
+		Content:  "content1",
+		Title:    "title1",
+		Author:   "author1",
+		Language: "language1",
+	}
+
+	db.MustExec("INSERT INTO archives(uuid, content, title, author, language ) VALUES (?, ?, ?, ?, ?)",
+		origin.UUID, origin.Content, origin.Title, origin.Author, origin.Language)
+
+	expect := models.Archive{
+		Content:  "content2",
+		Title:    "title2",
+		Author:   "author2",
+		Language: "language2",
+	}
+
+	var body bytes.Buffer
+	if err := json.NewEncoder(&body).Encode(&expect); err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest(http.MethodPut, "/archive/", &body)
+	rec := httptest.NewRecorder()
+	postArchive(rec, req)
+
+	if rec.Code != http.StatusCreated {
+		t.Errorf("status code must be 201 but: %d", rec.Code)
+	}
+
+	var uuid string
+	if err := json.NewDecoder(rec.Body).Decode(&uuid); err != nil {
+		t.Fatal(err)
+	}
+
+	var archive models.Archive
+	if err := db.Get(&archive, "SELECT content, title, author, language FROM archives WHERE uuid = ?", uuid); err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(archive, expect) {
+		t.Errorf("archive must be %v but %v", expect, archive)
+	}
+}
+
+func Test_deleteArchive(t *testing.T) {
+	db := sqlx.MustConnect("mysql", config.Config.DbAccess+"code_archives_test?parseTime=true&loc=Asia%2FTokyo")
+	defer func() {
+		// DBのCleanup
+		db.MustExec("set foreign_key_checks = 0")
+		db.MustExec("truncate table archives")
+		db.MustExec("set foreign_key_checks = 1")
+		db.Close()
+	}()
+
+	origin := models.Archive{
+		ID:        300,
+		UUID:      "uuid1",
+		Content:   "content1",
+		Title:     "title1",
+		Author:    "author1",
+		Language:  "language1",
+		CreatedAt: "2020-01-01",
+	}
+
+	db.MustExec("INSERT INTO archives(id, uuid, content, title, author, language, created_at ) VALUES (?, ?, ?, ?, ?, ?, ?)",
+		origin.ID, origin.UUID, origin.Content, origin.Title, origin.Author, origin.Language, origin.CreatedAt)
+
+	var body bytes.Buffer
+	req := httptest.NewRequest(http.MethodDelete, "/archive/uuid1", &body)
+	rec := httptest.NewRecorder()
+	deleteArchive(rec, req)
+
+	if rec.Code != http.StatusNoContent {
+		t.Errorf("status code must be 204 but: %d", rec.Code)
+	}
+
+	var archive models.Archive
+	if err := db.Get(&archive, "SELECT content, title, author, language FROM archives WHERE uuid = ?", origin.UUID); err != sql.ErrNoRows {
+		t.Fatal(err)
+	}
+
 }
